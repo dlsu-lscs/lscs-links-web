332cc994dc7b3350c3af2abb95eb668f1f54be6a
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-01-31 18:10:17 +0800

    fixed google login component

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,9 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "@/components/ui/toaster";
 
 const App = () => {
-<<<<<<< HEAD
   const clientID =
     (import.meta.env.VITE_APP_CLIENT_ID as string) ||
     "274365900184-jnvam3k9ce4o4i1bi00c0rmdrqcmtmf9.apps.googleusercontent.com";
-=======
-    const clientID =
-        (import.meta.env.REACT_APP_CLIENT_ID as string) ||
-        "631323246333-a20r4lll1rs1k93viaobh6f2neushf2t.apps.googleusercontent.com";
->>>>>>> 1cf46c4602c3d775fb5be9e08c4e1e9f8c03de18
 
     return (
         <>
