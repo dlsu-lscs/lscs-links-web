89079e83c5828644751383359ae24534435af225
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-01-31 18:19:16 +0800

    change google client id

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 const App = () => {
   const clientID =
     (import.meta.env.VITE_APP_CLIENT_ID as string) ||
-    "631323246333-l06jvts5smd7u24e2egdifn05qaqbodu.apps.googleusercontent.comcom";
+    "631323246333-l06jvts5smd7u24e2egdifn05qaqbodu.apps.googleusercontent.com";
 
     return (
         <>
