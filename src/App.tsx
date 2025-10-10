1b3286b8ec6e357db1ff086066ac54f8038ed10d
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-10-10 22:52:39 +0800

    hotfix: changed env default

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { Layout } from "./layout/Layout";
-import { BrowserRouter as Router } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Toaster } from "@/components/ui/toaster";
+import { Layout } from './layout/Layout'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Toaster } from '@/components/ui/toaster'
 
 const App = () => {
   const clientID =
     (import.meta.env.VITE_APP_CLIENT_ID as string) ||
-    "631323246333-l06jvts5smd7u24e2egdifn05qaqbodu.apps.googleusercontent.com";
+    '722891290777-o8g8kmt2g22omtalfsqfjsd06s8mt9h1.apps.googleusercontent.com'
 
-    return (
-        <>
-            <CookiesProvider>
-                <GoogleOAuthProvider clientId={clientID}>
-                    <Router>
-                        <Layout></Layout>
-                        <Toaster />
-                    </Router>
-                </GoogleOAuthProvider>
-            </CookiesProvider>
-        </>
-    );
-};
+  return (
+    <>
+      <CookiesProvider>
+        <GoogleOAuthProvider clientId={clientID}>
+          <Router>
+            <Layout></Layout>
+            <Toaster />
+          </Router>
+        </GoogleOAuthProvider>
+      </CookiesProvider>
+    </>
+  )
+}
 
-export default App;
+export default App
