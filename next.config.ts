994c7a1c3c337697c3e0db3173c67c1403358daa
ejsadiarin <ejs@ejsadiarin.com>
2026-01-14 22:27:22 +0800

    fix(CVE): nextjs version to 15.5.9

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const serverUrl = process.env.SERVER_API_URL || 'http://localhost:3000';
+
+if (!process.env.SERVER_API_URL) {
+  console.warn(
+    '\x1b[33m%s\x1b[0m',
+    'warn  - SERVER_API_URL is not defined in .env or .env.local. Defaulting to http://localhost:3000',
+  );
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/admin/:path*',
+        destination: `${serverUrl}/admin/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
   matcher: [
-    "/((?!api/auth|login|_next|_static|favicon.ico|public|.*\\.(?:png|jpg|jpeg|gif|svg|css|js)).*)",
+    "/((?!api/auth|login|_next|_static|_vercel|favicon.ico|public|.*\\.(?:png|jpg|jpeg|gif|svg|css|js|ico|txt|webp)).*)",
   ],
 };
