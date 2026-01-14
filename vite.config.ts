2482630c0f6558cec5baec8b60aabf9bfe140b98
ejsadiarin <ejs@ejsadiarin.com>
2026-01-14 16:36:13 +0800

    fix: core api url for auth

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/auth": {
-        target: "https://auth.app.dlsu-lscs.org",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/auth/, ""),
-      },
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/auth": {
+                target: "https://core.api.dlsu-lscs.org",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/auth/, ""),
+            },
+        },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-})
+});
+
